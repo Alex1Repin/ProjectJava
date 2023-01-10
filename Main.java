ca83e8561d8b7ae58f101759bb2a255757bc270f
Alex <al.repin.a@yandex.ru>
2023-01-10 22:08:53 +0300

    add Main

---
+++ Main.java
@@ -0,0 +1,13 @@
+package comp_shop;
+
+
+import java.io.IOException;
+import java.lang.ModuleLayer.Controller;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        comp_shop.Controller controllerMod = new comp_shop.Controller();
+    
+    }
+}
