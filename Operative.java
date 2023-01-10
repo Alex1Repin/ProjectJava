04169f1f8a14657d5a1ba6a3e5f535e6dcac4a58
Alex <al.repin.a@yandex.ru>
2023-01-10 22:13:12 +0300

    add operative

---
+++ Operative.java
@@ -0,0 +1,34 @@
+package comp_shop;
+
+public class Operative {
+    int size;
+    int freq;
+    int planks;  
+
+    Operative(int size, int freq, int planks){
+         this.size = size;
+         this.freq = freq;
+         this.planks = planks;
+    }
+    Operative(String[] params){
+        this.size = Integer.parseInt(params[0]);
+        this.freq = Integer.parseInt(params[1]);
+        this.planks =  Integer.parseInt(params[2]);
+    }
+    public String card(){
+        return "Memory>>" + "\n" + "Size, Gb " + this.size  + "\n" + "Frequency: "  + this.freq + "\n" + "planks: " + this.planks + "\n"; 
+    }
+    public boolean compare(Operative memo){
+        return this.size == memo.size && this.freq == memo.freq && this.planks == memo.planks;
+    }
+    public boolean checkSize(int size){
+        return this.size >= size;
+    }
+
+    public boolean checkFreq(int freq){
+        return this.freq >= freq;
+    }
+    public boolean filter(Operative memory){
+        return this.checkSize(memory.size) && this.checkFreq(memory.freq);
+    }
+}
